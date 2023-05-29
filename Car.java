a1c99e641034dec55633d660142d32ab6038539c
InfiniteDreamer1234 <130995647+InfiniteDreamer1234@users.noreply.github.com>
2023-05-29 15:14:42 -0400

    Added a background and a car

---
+++ Car.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    /**
+     * Act - do whatever the Car wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+     public void act()
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+           move(-3);
+           
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(3);
+            
+        }
+        
+    }
+    
+}
--- MyWorld.java
+++ MyWorld.java
@@ -13,9 +13,16 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    
+    GreenfootImage road = new GreenfootImage("Road.jpg"); 
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Car car = new Car();
+        addObject(car,400,250);
+        
+        
     }
 }
